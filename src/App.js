ec70edf605c89a5f0562754f0c610aa43b64741f
steeksg <steeksg@steeksg.com>
2021-02-03 14:31:24 +0300

    Refactoring

--- src/App.js
+++ src/App.js
@@ -9,32 +9,44 @@ function App() {
         <Table responsive striped bordered hover variant="dark">
           <thead>
             <tr>
-              <th className="w-25">Name training project</th>
-              <th className="w-25">Repository link</th>
-              <th className="w-25">View link</th>
-              <th className="w-25">Task link</th>
+              <th className="w-25">Stack</th>
+              <th className="w-25">Project</th>
+              <th className="w-25">Result</th>
+              <th className="w-25">Task</th>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td>hotel-training</td>
               <td>
-                <a href="https://github.com/steeksg/hotel-training">Github</a>
+                <a href="https://github.com/steeksg/hotel-training">
+                  hotel-training
+                </a>
               </td>
               <td>
-                <a href="https://steeksg.github.io/hotel-training">Github pages</a>
+                <a href="https://steeksg.github.io/hotel-training">
+                  Github pages
+                </a>
               </td>
-              <td><a href="https://github.com/steeksg/ghp/blob/master/src/tasks/hotel/hotel-training-task.pdf">Figma mockup</a></td>
+              <td>
+                <a href="https://github.com/steeksg/ghp/blob/master/src/tasks/hotel/hotel-training-task.pdf">
+                  Figma mockup
+                </a>
+              </td>
             </tr>
             <tr>
               <td>cadex-test</td>
               <td>
-                <a href="https://github.com/steeksg/cadex-test">Github</a>
+                <a href="https://github.com/steeksg/cadex-test">cadex-test</a>
               </td>
               <td>
                 <a href="https://cadex-test.herokuapp.com/">Heroku</a>
               </td>
-              <td><a href="https://github.com/steeksg/ghp/blob/master/src/tasks/cadex/cadex-test.pdf">Doc</a></td>
+              <td>
+                <a href="https://github.com/steeksg/ghp/blob/master/src/tasks/cadex/cadex-test.pdf">
+                  Doc
+                </a>
+              </td>
             </tr>
           </tbody>
         </Table>
