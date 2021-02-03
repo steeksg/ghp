d0f013e29204409046b2eb87b93e177b6cf27eb7
steeksg <steeksg@steeksg.com>
2021-02-03 14:09:38 +0300

    Refactoring

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           <thead>
             <tr>
               <th className="w-25">Name training project</th>
-              <th className="w-25">Repo link</th>
+              <th className="w-25">Repository link</th>
               <th className="w-25">View link</th>
               <th className="w-25">Task link</th>
             </tr>
@@ -19,22 +19,22 @@ function App() {
             <tr>
               <td>hotel-training</td>
               <td>
-                <a href="https://github.com/steeksg/hotel-training">REPO</a>
+                <a href="https://github.com/steeksg/hotel-training">Github</a>
               </td>
               <td>
-                <a href="https://steeksg.github.io/hotel-training">GHP</a>
+                <a href="https://steeksg.github.io/hotel-training">Github pages</a>
               </td>
               <td><a href="https://github.com/steeksg/ghp/blob/master/src/tasks/hotel/hotel-training-task.pdf">Figma mockup</a></td>
             </tr>
             <tr>
               <td>cadex-test</td>
               <td>
-                <a href="https://github.com/steeksg/cadex-test">REPO</a>
+                <a href="https://github.com/steeksg/cadex-test">Github</a>
               </td>
               <td>
                 <a href="https://cadex-test.herokuapp.com/">Heroku</a>
               </td>
-              <td><a href="https://github.com/steeksg/ghp/blob/master/src/tasks/cadex/cadex-test.pdf">Figma mockup</a></td>
+              <td><a href="https://github.com/steeksg/ghp/blob/master/src/tasks/cadex/cadex-test.pdf">Doc</a></td>
             </tr>
           </tbody>
         </Table>
